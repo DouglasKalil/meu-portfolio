8334a9da6cba47aeb6e6c8d31c9c659dd5046425
Douglas Kalil <137123741+DouglasKalil@users.noreply.github.com>
2025-05-14 19:00:18 -0300

    criação de portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+// Mostrar mais projetos
+document.getElementById('btn-ver-mais').addEventListener('click', function() {
+    document.querySelectorAll('.card-projeto.extra').forEach(card => {
+        card.style.display = 'block';
+    });
+    this.style.display = 'none'; // Esconde o botão depois de expandir
+});
+
+// Alternar Dark Mode
+document.getElementById('btn-darkmode').addEventListener('click', function() {
+    document.body.classList.toggle('darkmode');
+});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Animação para a aba "Sobre"
+    const sobre = document.querySelector('#conteudo-sobre');
+    if (sobre) {
+        sobre.classList.add('fade-in');
+    }
+
+    // Animação para a aba "Projetos"
+    const container = document.querySelector('.cards-projetos');
+    if (container) {
+        container.classList.add('animar-entrada');
+    }
+});
+
